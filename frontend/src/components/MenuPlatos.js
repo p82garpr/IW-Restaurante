ddd0ffe4f45ea5ad1dc429dba499dbda79a26ce1
delprado <i82gavaj@uco.es>
2024-04-12 19:55:26 +0200

    Primeros Pasos

--- frontend/src/components/MenuPlatos.js
+++ frontend/src/components/MenuPlatos.js
@@ -8,26 +8,26 @@ const MenuPlatos = () => {
     // Agrega más platos según sea necesario
   ];
 
+  const categorias = [...new Set(platos.map((plato) => plato.categoria))]; // Obtener categorías únicas
+
   return (
     <div className="menu-container">
       <nav className="menu-navigation">
-        <ul>
-          <li>Iniciar Sesión</li>
-          <li>Cesta</li>
-        </ul>
+        <button className="menu-button">Iniciar Sesión</button>
+        <button className="menu-button">Cesta</button>
       </nav>
 
       <div className="menu-section">
-        {platos.map((plato, index) => (
-          <div key={index} className="categoria-container">
-            <h2>{plato.categoria}</h2>
+        {categorias.map((categoria, index) => (
+          <div key={index} className="categoria-card">
+            <h2 className="categoria-titulo">{categoria}</h2>
             <div className="platos-container">
-              {platos.map((platoItem) => (
-                <div key={platoItem.id} className="plato-card">
-                  <img src={platoItem.imagen} alt={platoItem.nombre} />
+              {platos.filter((plato) => plato.categoria === categoria).map((plato) => (
+                <div key={plato.id} className="plato-card">
+                  <img src={plato.imagen} alt={plato.nombre} className="plato-imagen" />
                   <div className="plato-info">
-                    <h3>{platoItem.nombre}</h3>
-                    <p>Precio: ${platoItem.precio}</p>
+                    <h3 className="plato-nombre">{plato.nombre}</h3>
+                    <p className="plato-precio">Precio: ${plato.precio}</p>
                   </div>
                 </div>
               ))}
