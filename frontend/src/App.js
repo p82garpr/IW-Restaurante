9ffe1642dc8909a4d9044e341e9df1951150c773
Rafael García Pérez <p82garpr@uco.es>
2024-04-12 20:17:46 +0200

    categorias funcionan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MenuCategorias from './components/MenuCategorias';
 import Entrantes from './components/Entrantes';
-
+import Bebidas from './components/Bebidas';
+import Postres from './components/Postres';
+import Principales from './components/Principales';
 
 import Home from './components/Home';
 
@@ -16,6 +18,11 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route path="/components/MenuCategorias" component={MenuCategorias} />
           <Route path="/components/Entrantes" component={Entrantes} />
+          <Route path="/components/Bebidas" component={Bebidas} />
+          <Route path="/components/Postres" component={Postres} />
+          <Route path="/components/Principales" component={Principales} />
+          
+
         </Switch>
         
       </div>
