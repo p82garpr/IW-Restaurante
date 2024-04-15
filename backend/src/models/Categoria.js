e3c29b938414f80422de47f637839e0e80d532e8
Isaac Cejudo <p82ceali@uco.es>
2024-04-15 16:55:40 +0200

    Api categorias

--- backend/src/models/Categoria.js
+++ backend/src/models/Categoria.js
@@ -3,14 +3,11 @@ const {Schema, model} = require('mongoose')
 const categoriaSchema = new Schema({
     nombre: { type: String, enum: ['entrante', 'principal', 'postre', 'bebida'] },
     imagen: Buffer,
-    categoria: { type: String, enum: ['entrante', 'principal', 'postre', 'bebida'] },
-    descripcion: String, // Descripción específica para los productos de comida
-    ingredientes: { type: [String], default: [] } // Ingredientes específicos para los productos de comida
-    
+
         
 },
 {
     timestamps: true
 })
 
-module.exports = model('Producto', productoSchema)
+module.exports = model('Categoria', categoriaSchema)
