9383e97ae721d22e30621979c29a2e9605829518
Isaac Cejudo <p82ceali@uco.es>
2024-04-22 17:12:28 +0200

    Actualizacion de campo Imagen

--- backend/src/models/Categoria.js
+++ backend/src/models/Categoria.js
@@ -2,7 +2,7 @@ const {Schema, model} = require('mongoose')
 
 const categoriaSchema = new Schema({
     nombre: { type: String, enum: ['entrante', 'principal', 'postre', 'bebida'] },
-    imagen: Buffer,
+    imagen: String,
 
         
 },
--- backend/src/models/Producto.js
+++ backend/src/models/Producto.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose')
 const productoSchema = new Schema({
     nombre: String,
     precio: Number,
-    imagen: Buffer,
+    imagen: String,
     categoria: { type: Schema.Types.ObjectId, ref: 'Categoria' }, // Referencia a la categoría
     descripcion: String, // Descripción específica para los productos de comida
     ingredientes: { type: [String], default: [] } // Ingredientes específicos para los productos de comida
